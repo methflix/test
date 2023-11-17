ad9acc79679c7907e3118898c7c48f6040c8cd75
Deyusha69 <98688116+Deyusha69@users.noreply.github.com>
2023-11-18 02:19:49 +0800

    Create thorium.sh

---
+++ thorium.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Prompt user to select installation type
+echo "Please select the installation type:"
+echo "1. Thorium Browser with SSE3 support"
+echo "2. Thorium Browser without SSE3 support"
+read -p "Enter your choice (1/2): " choice
+
+# Install Thorium Browser with SSE3 support
+if [[ $choice -eq 1 ]]; then
+  paru -S thorium-browser-sse3-bin
+elif [[ $choice -eq 2 ]]; then
+  paru -S thorium-browser-bin
+else
+  echo "Invalid choice. Exiting script."
+  exit 1
+fi
+
+# Run Thorium Browser after installation
