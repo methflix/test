8f71a8ab1a049576980694f72319b0f9a6c2283d
Deyusha69 <98688116+Deyusha69@users.noreply.github.com>
2023-11-18 02:26:31 +0800

    Update thorium.sh

--- thorium.sh
+++ thorium.sh
@@ -8,9 +8,9 @@ read -p "Enter your choice (1/2): " choice
 
 # Install Thorium Browser with SSE3 support
 if [[ $choice -eq 1 ]]; then
-  paru -S thorium-browser-sse3-bin
+  paru -S thorium-browser-sse3-bin --skipreview --noconfirm
 elif [[ $choice -eq 2 ]]; then
-  paru -S thorium-browser-bin
+  paru -S thorium-browser-bin --skipreview --noconfirm
 else
   echo "Invalid choice. Exiting script."
   exit 1
