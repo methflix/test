042874df68f0084550253f9bcf592a0503a9449d
Deyusha69 <98688116+Deyusha69@users.noreply.github.com>
2023-11-18 16:01:37 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -21,3 +21,34 @@ case $choice in
         echo "Invalid choice. Installation canceled."
         ;;
 esac
+
+echo "Type N if you using a Desktop. Type y if you using a Laptop"
+echo "Do you want to proceed? [y/N]"
+read confirm
+
+if [ "$confirm" != "y" ]; then
+  echo "Aborting..."
+  exit 1
+fi
+
+sudo pacman -S wmctrl xdotool --noconfirm
+sudo gpasswd -a $USER input
+
+echo "Installing libinput-gestures using Paru..."
+paru -S libinput-gestures --skipreview --noconfirm
+```
+
+logo "Installing Themes"				 
+git clone https://github.com/Fausto-Korpsvart/Tokyo-Night-GTK-Theme.git
+cd Tokyo-Night-GTK-Theme/
+sudo cp -r themes/Tokyonight-Dark-BL-LB /usr/share/themes/
+cd
+printf "%s%sDone!!%s\n" "${BLD}" "${CGR}" "${CNC}"
+sleep 2	  
+clear
+
+logo "Setup TimeZone Asia/Manila.."
+sudo timedatectl set-timezone Asia/Manila >/dev/null 2>&1
+printf "%s%sSuccesfully!%s\n" "${BLD}" "${CGR}" "${CNC}"
+sleep 2
+clear
