1aad695327bab213d8df99681129e48c84e32dc5
Deyusha69 <98688116+Deyusha69@users.noreply.github.com>
2023-11-18 16:13:06 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
-# Prompt user to select installation type
-echo "Please select the installation type:"
-echo "1. Thorium Browser with SSE3 support"
-echo "2. Thorium Browser without SSE3 support"
-read -p "Enter your choice (1/2): " choice
+echo "Choose a Thorium Browser package to install:"
+echo "1. thorium-browser-sse3-bin"
+echo "2. thorium-browser-bin"
+echo "3. Cancel installation"
 
-# Install Thorium Browser with SSE3 support
-if [[ $choice -eq 1 ]]; then
-  paru -S thorium-browser-sse3-bin --skipreview --noconfirm
-elif [[ $choice -eq 2 ]]; then
-  paru -S thorium-browser-bin --skipreview --noconfirm
-else
-  echo "Invalid choice. Exiting script."
-  exit 1
-fi
+read -p "Enter your choice (1, 2, or 3): " choice
 
-echo "Type N if you using a Desktop. Type y if you using a Laptop"
-echo "Do you want to proceed? [y/N]"
-read confirm
-
-if [ "$confirm" != "y" ]; then
-  echo "Aborting..."
-  exit 1
-fi
+case $choice in
+    1)
+        paru -S thorium-browser-sse3-bin
+        ;;
+    2)
+        paru -S thorium-browser-bin
+        ;;
+    3)
+        echo "Installation canceled."
+        ;;
+    *)
+        echo "Invalid choice. Installation canceled."
+        ;;
+esac
 
 echo "This script will install the following packages and add the user to the 'input' group:"
 echo "1. wmctrl"
